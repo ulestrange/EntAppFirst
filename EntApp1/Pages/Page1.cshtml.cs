353a689e8d09baf915fa6213af283f6a3e45cf58
ulestrange <lestrange.una@itsligo.ie>
2019-01-25 15:06:39 +0000

    End of video the PageModel class

---
+++ EntApp1/Pages/Page1.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Hello World from the Get method";
+        }
+    }
+}
--- EntApp1/Startup.cs
+++ EntApp1/Startup.cs
@@ -15,6 +15,8 @@ namespace EntApp1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +28,8 @@ namespace EntApp1
             }
 
             app.UseStaticFiles();
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
