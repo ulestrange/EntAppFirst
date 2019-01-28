764e3563116e6642a5288fcc3aedf26fbbf3a098
ulestrange <lestrange.una@itsligo.ie>
2019-01-28 17:43:56 +0000

    Some changes for worksheet1

---
+++ EntApp1/Pages/ws1.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Pages
+{
+    public class ws1Model : PageModel
+    {
+
+        public string[] MyTestArray { get; set; } 
+            = { "England", "Ireland", "Scotland", "Wales" };
+
+        public string Message { get; set; } = "";
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost (string firstname)
+        {
+            Message = $"Hello {firstname}";
+        }
+    }
+}
