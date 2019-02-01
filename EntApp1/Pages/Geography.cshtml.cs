b782ccf5766739aa8072b67ad01bdf2a4b308dd9
Una LEstrange <lestrange.una@itsligo.ie>
2019-02-01 19:37:15 +0000

    Multiple selecton list

--- EntApp1/Pages/Geography.cshtml.cs
+++ EntApp1/Pages/Geography.cshtml.cs
@@ -13,11 +13,38 @@ namespace EntApp1.Pages
         public string[] ProvinceList { get; set; }
             = { "Connaught", "Leinster", "Munster", "Ulster" };
 
+        public string[] CountyList { get; set; } = { "Sligo", "Letrim", "Mayo", "Galway", "Roscommon" };
+
+
         [BindProperty]
         public bool[] ProvinceChoice { get; set; } = new bool[4];
 
+        [BindProperty]
+        public string[] ChosenCounties { get; set; } = { };
+
+        public string Message { get; set; } = "";
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            if (ChosenCounties.Length == 0)
+            {
+                Message = "Your picked no counties";
+            }
+            else
+            {
+                Message = $"<ul>";
+                foreach (string county in ChosenCounties)
+                {
+                    Message += $"<li>{county}</li>";
+                }
+
+                Message += "<ul>";
+            }
+
         }
 
 
