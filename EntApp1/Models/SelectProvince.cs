5c0c3644d2576d53b15e816782782ec296b6b197
Una LEstrange <lestrange.una@itsligo.ie>
2019-02-04 18:56:53 +0000

    After the video on multiple selection using a List<SelectItems>

---
+++ EntApp1/Models/SelectProvince.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntApp1.Models
+{
+
+    /* This class holds the name of a province and whether it has been chosen
+     */
+    public class ProvincesSelected
+    {
+
+        public string ProvinceName { get; set; }
+        public bool IsChosen { get; set; }
+
+        public ProvincesSelected(string provinceName, bool isChosen)
+        {
+            ProvinceName = provinceName;
+            IsChosen = isChosen;
+        }
+
+        public ProvincesSelected()
+        {
+            ProvinceName = "";
+            IsChosen = false;
+        }
+
+
+    }
+}
+
--- EntApp1/Pages/Geography.cshtml.cs
+++ EntApp1/Pages/Geography.cshtml.cs
@@ -2,22 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EntApp1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EntApp1.Pages
 {
     public class GeographyModel : PageModel
     {
 
-        public string[] ProvinceList { get; set; }
-            = { "Connaught", "Leinster", "Munster", "Ulster" };
-
-        public string[] CountyList { get; set; } = { "Sligo", "Letrim", "Mayo", "Galway", "Roscommon" };
-
-
         [BindProperty]
-        public bool[] ProvinceChoice { get; set; } = new bool[4];
+
+        public ProvincesSelected[] Provinces { get; set; }
+        = {new ProvincesSelected("Connaught", false),
+            new ProvincesSelected("Leinster", false),
+            new ProvincesSelected("Ulster", false),
+            new ProvincesSelected("Munster", false)
+        };
+
+
+        //public string[] ProvinceList { get; set; }
+        //    = { "Connaught", "Leinster", "Munster", "Ulster" };
+
+        public List<SelectListItem> CountyList { get; set; } =
+
+            new List<SelectListItem>
+            { new SelectListItem ("Sligo", "Sligo" ),
+               new SelectListItem ("Letrim", "Leitrim" ),
+                              new SelectListItem ("Mayo", "Mayo" ),
+                                             new SelectListItem ("Galway", "Galway" ),
+                                                            new SelectListItem ("Roscommon", "Roscommon" ) };
+
+
+
+        //[BindProperty]
+        //public bool[] ProvinceChoice { get; set; } = new bool[4];
 
         [BindProperty]
         public string[] ChosenCounties { get; set; } = { };
@@ -26,27 +46,18 @@ namespace EntApp1.Pages
 
         public void OnGet()
         {
+
+
         }
 
         public void OnPost()
         {
-            if (ChosenCounties.Length == 0)
-            {
-                Message = "Your picked no counties";
-            }
-            else
-            {
-                Message = $"<ul>";
-                foreach (string county in ChosenCounties)
-                {
-                    Message += $"<li>{county}</li>";
-                }
 
-                Message += "<ul>";
-            }
 
         }
 
 
     }
+
+
 }
