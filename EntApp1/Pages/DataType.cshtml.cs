c257965b6c956a1e09d43e566ba6f3b1e7dd9622
ulestrange <lestrange.una@itsligo.ie>
2019-02-08 13:34:47 +0000

    Workshet 3 done: two new pages Time and DataType added

---
+++ EntApp1/Pages/DataType.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Pages
+{
+    public class DataTypeModel : PageModel
+    {
+        [BindProperty]
+        [DataType(DataType.CreditCard)]
+        public string CreditCard { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Currency)]
+        public string Currency { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Duration)]
+        public string Duration { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Html)]
+        public string Html{ get; set; }
+        [BindProperty]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+        [BindProperty]
+        [DataType(DataType.MultilineText)]
+        public string MlText { get; set; }
+        [BindProperty]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+
+        [BindProperty]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+
+        [BindProperty]
+        [DataType(DataType.Url)]
+        public string WebAddress { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Upload)]
+        public string Upload { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ EntApp1/Pages/Time.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EntApp1.Pages
+{
+    public class TimeModel : PageModel
+    {
+
+        public string[] DayNames { get; set; } = { "Mon", "Tues", "Wed", "Thurs", "Fri" };
+
+        [BindProperty]
+        public bool[] IsDayChosen { get; set; } = new bool[5];
+
+        [BindProperty]
+
+        public string[] MonthsChosen { get; set; } = { };
+
+        public List<SelectListItem> MonthsSelectList { get; set; } =
+
+            new List<SelectListItem>
+            { new SelectListItem ("Jan", "Jan" ),
+               new SelectListItem ("Feb", "Feb" ),
+               new SelectListItem ("March", "March" ),
+                new SelectListItem ("April", "April" ),
+                new SelectListItem ("May", "May" ) };
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
